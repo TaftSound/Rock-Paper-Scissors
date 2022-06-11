d64ceeb383462077eadb5689d731dd65500dea10
TaftSound <mattmurphytattoos@gmail.com>
2022-06-11 09:33:53 -0700

    Add gameplay round function

--- javascript.js
+++ javascript.js
@@ -15,4 +15,33 @@ function computerPlay()
     }
 }
 
-console.log(computerPlay());
+function playRound( computerSelection, playerSelection )
+{
+    let comp = computerSelection.toUpperCase();
+    let player = playerSelection.toUpperCase();
+    if ( comp === player )
+    {
+        return `Computer: ${comp}  Player: ${player}\n The game is a tie!`;
+    }
+    else if ( (comp === "ROCK") && (player === "SCISSORS") )
+    {
+            return `Computer: ${comp}  Player: ${player}\n You lose! ${comp} beats ${player}`;
+    }
+    else if ( (comp === "SCISSORS") && (player === "PAPER") )
+    {
+            return `Computer: ${comp}  Player: ${player}\n You lose! ${comp} beats ${player}`;
+    }
+    else if ( (comp === "PAPER") && (player === "ROCK") )
+    {
+            return `Computer: ${comp}  Player: ${player}\n You lose! ${comp} beats ${player}`;
+    }
+    else
+    {
+        return `Computer: ${comp}  Player: ${player}\n You win! ${player} beats ${comp}`;
+    }
+}
+
+let computerSelection = computerPlay();
+console.log(computerSelection);
+let result = playRound(computerSelection, "PAPER");
+console.log(result);
