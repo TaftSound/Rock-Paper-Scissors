853a4f7c48736a6c73fc98635d5f06a509fa44c6
TaftSound <mattmurphytattoos@gmail.com>
2022-06-19 10:31:38 -0700

    Create begin button and round number message

--- javascript.js
+++ javascript.js
@@ -1,7 +1,6 @@
 // Declare global variables
 let numAttempts = -1;
-let compScore = 0;
-let playerScore = 0;
+
 let resetRecursion = false;
 
 // Generates computer's move choice
@@ -28,23 +27,23 @@ function capFirstLetter(input) {
 }
 
 // Prompts for and stores player move choice
-function playerInput() {
-    let input = prompt("Rock, Paper, or Scissors?").toLowerCase();
-    if ( (input !== "rock")
-        && (input !== "scissors")
-        && (input !== "paper") ) {
-        alert("Please make a valid selection!");
-    // Below is used to exit recursion and limit num of functions on call stack
-        ++numAttempts;
-        if (numAttempts < 5) { return playerInput(); }
-        else { resetRecursion = true; return }; 
-    }
-    else {
-    resetRecursion = false;
-    numAttempts = -1;
-    return capFirstLetter(input);
-    }
-}
+// function playerInput() {
+//     let input = prompt("Rock, Paper, or Scissors?").toLowerCase();
+//     if ( (input !== "rock")
+//         && (input !== "scissors")
+//         && (input !== "paper") ) {
+//         alert("Please make a valid selection!");
+//     // Below is used to exit recursion and limit num of functions on call stack
+//         ++numAttempts;
+//         if (numAttempts < 5) { return playerInput(); }
+//         else { resetRecursion = true; return }; 
+//     }
+//     else {
+//     resetRecursion = false;
+//     numAttempts = -1;
+//     return capFirstLetter(input);
+//     }
+// }
 
 // Function to manage a single round of play and return results
 function playRound() {
@@ -86,4 +85,136 @@ function game() {
     else { console.log("The game ends in a tie!"); }
 }
 
-game();
+
+// Define gameplay function
+// ============================================================
+const beginButton = document.querySelector("#begin");
+const sectionOne = document.querySelector(".one");
+const sectionTwo = document.querySelector(".two");
+const sectionThree = document.querySelector(".three");
+let playerScore = 0;
+let computerScore = 0;
+let roundMessage = "";
+
+beginButton.classList.add('fade-in');
+
+beginButton.addEventListener('click', () => {
+    removeItemMedium(beginButton);
+    setTimeout( () => { playGame() }, 500 );
+});
+
+function playGame() {
+    roundMessage = createRoundMessage(1);
+    sectionOne.appendChild(roundMessage);
+    roundMessage.classList.add('fade-in');
+}
+
+// function playGame() {
+//     let i = 1;
+//     while (i <= 5) {
+//         console.log(i);
+//         roundMessage = createRoundMessage(i);
+//         setTimeout( () => { 
+//             sectionOne.appendChild(roundMessage);
+//             roundMessage.classList.add('fade-in');
+//         }, 250 );
+
+//         setTimeout( () => { 
+//             removeItemSlow(roundMessage); 
+//         }, 750 );
+        
+//         setTimeout( () => { 
+//             createGameRound();
+//         }, 2500);
+//     }
+// }
+
+
+
+
+// Create's new round message
+// ============================================================
+function createRoundMessage(number) {
+    const roundAlertHeader = document.createElement('h2');
+    roundAlertHeader.setAttribute('class', 'message');
+    roundAlertHeader.innerText = `Round: ${number}`;
+    return roundAlertHeader;
+}
+
+function createGameMessage(message) {
+    const gameMessage = document.createElement('h2');
+    gameMessage.classList.add('message');
+    gameMessage.classList.add('small');
+    gameMessage.innerText = message;
+    return gameMessage;
+}
+
+// Create button choice group
+// ============================================================
+function createButton(id) {
+    const button = document.createElement('button');
+    button.setAttribute('id', id.toLowerCase());
+    button.innerText = id;
+    return button;
+}
+
+function createGameRound() {
+    const cardSectionTwo = document.querySelector('.two');
+    const buttonGroup = document.createElement('div');
+    const rockButton = createButton('Rock');
+    const paperButton = createButton('Paper');
+    const scissorsButton = createButton('Scissors');
+    const playMessage = createGameMessage('Make Your Move:');
+    let choice = "";
+
+    cardSectionTwo.appendChild(buttonGroup);
+    buttonGroup.appendChild(rockButton);
+    rockButton.classList.add('fade-in');
+    buttonGroup.appendChild(paperButton);
+    paperButton.classList.add('fade-in');
+    buttonGroup.appendChild(scissorsButton);
+    scissorsButton.classList.add('fade-in');
+    buttonGroup.setAttribute('id', 'button-group');
+    sectionOne.appendChild(playMessage);
+    playMessage.classList.add('fade-in');
+
+
+    rockButton.addEventListener('click', () => { 
+        removeButtonGroup(rockButton, paperButton, scissorsButton);
+        choice = "rock"; 
+    });
+    paperButton.addEventListener('click', () => { 
+        removeButtonGroup(paperButton, rockButton, scissorsButton);
+        choice = "paper";
+    });
+    scissorsButton.addEventListener('click', () => { 
+        removeButtonGroup(scissorsButton, paperButton, rockButton);
+        choice = "scissors";
+    });
+        removeItemMedium(playMessage);
+        return choice;
+}
+
+function removeButtonGroup(buttonClicked, buttonTwo, buttonThree) {
+    removeItemMedium(buttonClicked);
+    removeItem(buttonTwo);
+    removeItem(buttonThree);
+}
+
+function removeItem(item) {
+    item.classList.add('disappear');
+    setTimeout( () => { item.remove() }, 450 );
+}
+
+function removeItemMedium(item) {
+    setTimeout( () => { item.classList.add('disappear') }, 200 );
+    setTimeout( () => { item.remove() }, 450 );
+}
+
+function removeItemSlow(item) {
+    setTimeout( () => { item.classList.add('disappear-slow') }, 500 );
+    setTimeout( () => { item.remove() }, 1500 );
+}
+
+
+// game();
